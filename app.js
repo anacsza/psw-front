943cbcfa2883811bfe5dc70df0c69582a8f30556
anacsza <ana.caroline.souza09@gmail.com>
2025-07-05 11:29:54 -0300

    feat: Created the page from mock exercise

---
+++ app.js
@@ -0,0 +1,7 @@
+document.getElementById('edit-button').addEventListener('click', function() {
+  alert('Button 1 clicked!');
+});
+
+document.getElementById('delete-button').addEventListener('click', function() {
+  alert('Button 2 clicked!');
+});
