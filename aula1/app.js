8561a9a2969b485e9ea665605ecd019db456f5ee
anacsza <ana.caroline.souza09@gmail.com>
2025-07-19 09:07:24 -0300

    feat: organize the files

---
+++ aula1/app.js
@@ -0,0 +1,7 @@
+document.getElementById('edit-button').addEventListener('click', function () {
+    alert('Alterado');
+});
+
+document.getElementById('delete-button').addEventListener('click', function () {
+    alert('Excluído');
+});
