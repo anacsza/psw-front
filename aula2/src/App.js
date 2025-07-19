7cc8d4661b4c4d85acb7ea8d7d38ae6311296bd8
anacsza <ana.caroline.souza09@gmail.com>
2025-07-19 10:50:32 -0300

    feat: migrate aula1 to react

--- aula2/src/App.js
+++ aula2/src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  // Event handlers for buttons
+  const handleEdit = () => {
+    alert('Alterado');
+  };
+
+  const handleDelete = () => {
+    alert('Excluído');
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     <main className="Main-container">
+        <figure className="Image-container">
+            <img className="Image-container-img" src="/iphone.jpg" alt="Smartphone IPhone XPTO"></img>
+        </figure>
+        <section className="Info-container">
+            <h1 className="Info-title">(1234) Smartphone IPhone XPTO</h1>
+            <p className="Info-description">Eletrônicos</p>
+            <p className="Info-price">R$3.000,00</p>
+            <nav className="Button-container">
+                <button className="Button" onClick={handleEdit}>Editar</button>
+                <button className="Button" onClick={handleDelete}>Excluir</button>
+            </nav>
+        </section>
+    </main>
   );
 }
 
